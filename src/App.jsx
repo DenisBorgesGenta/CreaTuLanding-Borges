88c3d1cf93e25f23e9e576ea40a5fbb64e638c04
DenisBorgesGenta <144848239+DenisBorgesGenta@users.noreply.github.com>
2023-12-19 18:35:54 -0300

    Edit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Button from './assets/components/Button'
+
+function App() {
+
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>Hello. Hi. I'm the problem it's me.</div>
+  )
+}
+
+export default Button
